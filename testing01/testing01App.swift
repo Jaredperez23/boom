3a36869819b1965ae952b87b90cafca903e34fbe
jaredperez23 <jaredperez5623@gmail.com>
2024-07-18 18:55:24 -0700

    Initial Commit

---
+++ testing01/testing01App.swift
@@ -0,0 +1,17 @@
+//
+//  testing01App.swift
+//  testing01
+//
+//  Created by Jared Perez on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct testing01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
